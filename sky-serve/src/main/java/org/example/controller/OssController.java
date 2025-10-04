9570f0fd2cfdd218d5d8e511e0d761ed355a9a36
inno <lemonkanghao@gmail.com>
2025-10-04 15:37:31 +0800

    强制上传

--- sky-serve/src/main/java/org/example/controller/OssController.java
+++ sky-serve/src/main/java/org/example/controller/OssController.java
@@ -65,7 +65,7 @@ public class OssController {
     public void getExcel(HttpServletResponse response) throws IOException {
         log.info("getExcel");
         List<ExcelUserOrdersDto> list=new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 11; i++) {
             ExcelUserOrdersDto excelUserOrdersDto=new ExcelUserOrdersDto();
             excelUserOrdersDto.setOrderDate(LocalDate.now());
             excelUserOrdersDto.setEmployeName(String.valueOf(i));
